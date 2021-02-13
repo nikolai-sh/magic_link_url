03d2e2dbbb8b3b4fc2b9793c9f8d362c06df9f2c
Nikolay <nikolain073@gmail.com>
2021-02-13 20:35:22 +0200

    Add message tegs

--- magic_link/settings.py
+++ magic_link/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-
+from django.contrib import messages
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -128,3 +128,12 @@ STATICFILES_DIRS = [
 
 #Custom User model
 AUTH_USER_MODEL = 'magic_link_auth.User'
+
+#Message tags
+MESSAGE_TAGS = {
+    messages.DEBUG: 'debug',
+    messages.INFO: 'info',
+    messages.SUCCESS: 'success',
+    messages.WARNING: 'warning',
+    messages.ERROR: 'danger',
+}
