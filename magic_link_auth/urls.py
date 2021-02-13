58b68f04bdb45290a645b97fd09403bc70e05691
Nikolay <nikolain073@gmail.com>
2021-02-13 20:04:45 +0200

    Update login/token url

--- magic_link_auth/urls.py
+++ magic_link_auth/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('register/', views.register, name='register'),
     path('send-magic-link/', views.send_magic_link, name='send-magic-link'),
-    path('login/token/', views.login_magic_link),
+    path('login/token/', views.login_with_magic_link),
 
 ]
